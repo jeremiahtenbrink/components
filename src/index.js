c30fe7c1c9e7bf46b7a954dbd016cc7e7f5a9766
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-01-28 20:45:31 -0700

    Played around with eslint trying to get eslint to work with airbnb and react.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
-const App = () => {
-    return (
-        <div className="ui container comments">
-            <CommentDetail author="Sam"
-                           timeAgo="Today at 4:45PM"
-                           comment="First comment"
-                           avatar={ faker.image.avatar() } />
-            <CommentDetail author="Alex"
-                           timeAgo="Today at 2:00AM"
-                           comment="Second comment"
-                           avatar={ faker.image.avatar() } />
-            <CommentDetail author="Jane"
-                           timeAgo="Yesterday at 1:56PM"
-                           comment="Third comment"
-                           avatar={ faker.image.avatar() } />
-        </div>
-    );
-};
+const App = () => (
+    <div className="ui container comments">
+        <CommentDetail
+            author="Sam"
+            timeAgo="Today at 4:45PM"
+            comment="First comment"
+            avatar={ faker.image.avatar() }
+        />
+        <CommentDetail
+            author="Alex"
+            timeAgo="Today at 2:00AM"
+            comment="Second comment"
+            avatar={ faker.image.avatar() }
+        />
+        <CommentDetail
+            author="Jane"
+            timeAgo="Yesterday at 1:56PM"
+            comment="Third comment"
+            avatar={ faker.image.avatar() }
+        />
+    </div>
+);
 
-ReactDOM.render( <App />, document.querySelector( '#root' ) );
+ReactDOM.render( <App />, document.querySelector( "#root" ) );
